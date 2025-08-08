42d00bbb1d7fcdecd50589c655dfe979aaa9f5a8
tatiana.maldonado <tatiana.maldonado@iesonline.com.co>
2025-08-08 07:58:46 -0500

    feat: :sparkles: Agrega menú de consola para interactuar con el usuario

--- src/main/java/com/alura/literatura/LiteraturaApplication.java
+++ src/main/java/com/alura/literatura/LiteraturaApplication.java
@@ -1,13 +1,39 @@
 package com.alura.literatura;
 
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraturaApplication {
+public class LiteraturaApplication  implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraturaApplication.class, args);
+	}
+
+	@Override
+    public void run(String... args) {
+        Scanner teclado = new Scanner(System.in);
+        int opcion = -1;
+
+        while (opcion != 0) {
+            System.out.println("\n--- MENÚ ---");
+            System.out.println("1 - Buscar libro por título");
+            System.out.println("2 - Listar todos los libros");
+            System.out.println("3 - Listar autores");
+            System.out.println("4 - Autores vivos en un año");
+            System.out.println("5 - Cantidad de libros por idioma");
+            System.out.println("0 - Salir");
+            System.out.print("Opción: ");
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            // Llamar métodos correspondientes
+        }
+
+        System.out.println("¡Gracias por usar LiterAlura!");
 	}
 
 }
